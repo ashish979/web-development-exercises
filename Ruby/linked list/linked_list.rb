fe8d1f5efd94965ed54214f28fcb9fa97dd77b0c
Ashish Agrawal <ashish.agrawal@payu.in>
2017-02-24 13:21:32 +0530

    adds method for insertion and deletion from a singly linked list and rvmrc

--- Ruby/linked list/linked_list.rb
+++ Ruby/linked list/linked_list.rb
@@ -21,16 +21,50 @@ class LinkedList
 
   def traverse_list(head)
     current = head
+    arr = []
     while current != nil
-      puts current.datum
+      arr << current.datum
       current = current.next_node
     end
+    puts arr.join(', ')
+  end
+
+  def insert_element(head, data, position)
+    current = head
+    k = 1
+    while k < position
+      current = current.next_node
+      k += 1
+    end
+    new_node = Node.new(data, nil)
+    new_node.next_node = current.next_node
+    current.next_node = new_node
+    head
+  end
+
+  def delete_element(head, position)
+    p, q = head, head.next_node
+    k = 2
+    while k < position
+      p = p.next_node
+      q = q.next_node
+      k += 1
+    end
+    p.next_node = q.next_node
+    q.next_node = nil
+    head
   end
 end
 
 l = LinkedList.new
 arr = l.get_input
+
 head = l.create_list(arr)
 l.traverse_list(head)
 
+head = l.insert_element(head, 4, 3)
+l.traverse_list(head)
+
+head = l.delete_element(head, 4)
+l.traverse_list(head)
 
